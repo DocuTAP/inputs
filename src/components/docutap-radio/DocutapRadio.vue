2e2104a7116df2dd06756fb2930607f2cbf6be14
adriancarriger <acarriger@docutap.com>
2017-10-23 12:52:00 -0500

    feat(radio): add disabled state

--- src/components/docutap-radio/DocutapRadio.vue
+++ src/components/docutap-radio/DocutapRadio.vue
@@ -1,7 +1,8 @@
 <template>
   <div :class="{'md-input-invalid': errors.has(schema.name)}" class="md-radio-group">
     <div class="md-radio-group-label" v-if="schema.label">{{schema.label}} <span v-if="errors.has(schema.name)"> {{errors.items[0].rule === 'required' ? 'is required' : 'not valid'}}</span></div>
-    <md-radio @change="onChange" v-for="(value, key) in schema.values" :key="value" :md-value="value" v-model="model[schema.name]" :name="schema.name" v-validate="radioValidator(key)">{{value}}</md-radio>
+    <md-radio @change="onChange" v-for="(value, key) in schema.values" :key="value" :disabled="schema.disabled" :md-value="value" v-model="model[schema.name]" :name="schema.name" v-validate="radioValidator(key)">{{value}}</md-radio>
+    <md-button v-if="schema.disabledButton" @click.native="schema.disabled = !schema.disabled" :fauxLink="true">{{schema.disabledButton}}</md-button>
   </div>
 </template>
 
