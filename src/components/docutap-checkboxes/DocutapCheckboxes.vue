f80bd082b8214123868ab8d76943eba85422ce7a
Adrian Carriger <hello@adriancarriger.com>
2018-04-06 12:08:02 -0500

    Deploy next version

--- src/components/docutap-checkboxes/DocutapCheckboxes.vue
+++ src/components/docutap-checkboxes/DocutapCheckboxes.vue
@@ -1,7 +1,8 @@
 <template>
   <div :class="{'md-input-invalid': errors.has(schema.name)}" class="md-checkboxes-group">
     <div class="md-checkboxes-group-label" v-if="schema.label && (!schema.hideLabel || errors.has(schema.name))">{{schema.label}} <span v-if="errors.has(schema.name)"> {{errors.items[0].rule === 'required' ? 'is required' : 'not valid'}}</span></div>
-    <md-checkbox v-for="(value, key) in schema.values" :key="value" :name="`${schema.name}-${key}`" @change="onChange" v-model="model[schema.name][value]">{{value}}</md-checkbox>
+    <md-checkbox v-for="(value, key) in schema.values" :key="value" :name="`${schema.name}-${key}`" :disabled="schema.disabled" @change="onChange" v-model="model[schema.name][value]">{{value}}</md-checkbox>
+    <md-button v-if="schema.disabledButton" @click.native="schema.disabled = !schema.disabled" :fauxLink="true">{{schema.disabledButton}}</md-button>
     <input type="hidden" :name="schema.name" v-model="fauxInputValue" v-validate="schema.validator" />
   </div>
 </template>
--- src/components/docutap-select/DocutapSelect.vue
+++ src/components/docutap-select/DocutapSelect.vue
@@ -1,9 +1,10 @@
 <template>
   <md-input-container :class="{'md-input-invalid': errors.has(schema.name)}">
     <label v-if="schema.label">{{schema.label}} <span v-if="errors.has(schema.name)"> {{errors.items[0].rule === 'required' ? 'is required' : 'not valid'}}</span></label>
-    <md-select @input="onInput" v-validate="schema.validator" :name="schema.name" v-model="model[schema.name]">
+    <md-select @input="onInput" v-validate="schema.validator" :name="schema.name" :disabled="schema.disabled" v-model="model[schema.name]">
       <md-option v-for="value in schema.values" :key="value" :value="value">{{value}}</md-option>
     </md-select>
+    <md-button v-if="schema.disabledButton" @click.native="schema.disabled = !schema.disabled" :fauxLink="true">{{schema.disabledButton}}</md-button>
   </md-input-container>
 </template>
 
