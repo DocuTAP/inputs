32ff48a182f811eeaeb327fc8300113f0dfc68c7
Adrian Carriger <hello@adriancarriger.com>
2018-05-22 08:50:20 -0500

    feat(input): validate on mount
    Jira: COF-62

--- src/components/docutap-input/DocutapInput.vue
+++ src/components/docutap-input/DocutapInput.vue
@@ -1,6 +1,6 @@
 <template>
   <md-input-container :class="{'md-input-invalid': errors.has(schema.name) && showingErrors}">
-    <label v-if="schema.label">{{schema.label}} <span v-if="error && showingErrors"> {{error.rule === 'required' ? 'is required' : 'not valid'}}</span></label>
+    <label v-if="schema.label">{{schema.label}} <span v-if="error && showingErrors"> {{errorMessage(error)}}</span></label>
     <md-input :scope="schema.scope" v-validate="validator" :disabled="schema.disabled" :phonePattern="schema.phonePattern" :name="schema.name" :type="schema.type" v-model="model[schema.name]" @input="onInput" @focus.native="onFocus" @blur.native="onBlur" :placeholder="schema.placeholder"></md-input>
     <md-button v-if="schema.disabledButton" @click.native="schema.disabled = !schema.disabled" :fauxLink="true">{{schema.disabledButton}}</md-button>
   </md-input-container>
@@ -35,7 +35,18 @@ export default {
     // if the field that has been touched
     // or the submit button was pressed.
     showingErrors () {
-      return this.showErrors || (!this.isFocsed && this.touched)
+      if (this.showErrors) {
+        return true
+      }
+
+      if (this.schema.showErrorsIfDirtyOrUnfocused) {
+        return this.touched || !this.isFocsed
+      }
+
+      return (
+        (!this.isFocsed || this.schema.showErrorsIfFocused) &&
+        (this.touched || this.schema.showErrorsIfPristine)
+      )
     },
     error () {
       return this.errors.items.find(item => item.field === this.schema.name)
@@ -43,10 +54,10 @@ export default {
   },
   methods: {
     onInput (input) {
-      if (input.length) { this.touched = true }
-      this.$nextTick(() => {
-        this.$validator.validate(this.schema.name, this.model[this.schema.name])
-      })
+      if (input.length) {
+        this.touched = true
+      }
+      this.validate()
       this.$emit('input', this.mdValue, input)
     },
     onFocus () {
@@ -55,11 +66,32 @@ export default {
     onBlur (input) {
       this.isFocsed = false
       this.blured = true
+    },
+    validate () {
+      this.$nextTick(() => {
+        this.$validator.validate(this.schema.name, this.model[this.schema.name])
+      })
+    },
+    errorMessage (error) {
+      if (error.rule === 'required') {
+        return 'is required'
+      }
+
+      if (
+        this.schema.errorMessages !== undefined &&
+        error.rule in this.schema.errorMessages
+      ) {
+        return this.schema.errorMessages[error.rule]
+      }
+
+      return 'not valid'
     }
   },
   mounted () {
     if (this.schema.type === 'tel') {
-      const phonePattern = this.schema.phonePattern ? this.schema.phonePattern.length : 'XXX-XXX-XXXX'
+      const phonePattern = this.schema.phonePattern
+        ? this.schema.phonePattern.length
+        : 'XXX-XXX-XXXX'
       const validators = this.schema.validator.split('|')
       validators.push(`min:${phonePattern.length}`)
       validators.push(`max:${phonePattern.length}`)
@@ -67,6 +99,7 @@ export default {
     } else {
       this.validator = this.schema.validator
     }
+    this.validate()
   }
 }
 </script>
